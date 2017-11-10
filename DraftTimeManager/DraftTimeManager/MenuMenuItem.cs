fd1b97aaaa676078410ee3d1e3017d100df69073
tomohsia itaya <tomopy03@gmail.com>
2017-11-10 13:45:19 +0900

    initial

---
+++ DraftTimeManager/DraftTimeManager/MenuMenuItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DraftTimeManager
+{
+
+    public class MenuMenuItem
+    {
+        public MenuMenuItem()
+        {
+            TargetType = typeof(MenuDetail);
+        }
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public Type TargetType { get; set; }
+    }
+}
