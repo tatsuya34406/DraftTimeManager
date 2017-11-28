cb822b6bf1cb0648146b0bbd3cbbde042a5a48e0
tomopy03 <tomopy03@gmail.com>
2017-11-29 07:55:37 +0900

    Create ConnectionModel

---
+++ DraftTimeManager/DraftTimeManager/Models/ConnectionModel.cs
@@ -0,0 +1,43 @@
+﻿using PCLStorage;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace DraftTimeManager.Models
+{
+    public class ConnectionModel
+    {
+        public ConnectionModel()
+        {
+        }
+
+        private async Task<SQLiteConnection> CreateConnection()
+        {
+            const string DatabaseFileName = "item.db3";
+            // ルートフォルダを取得する
+            IFolder rootFolder = FileSystem.Current.LocalStorage;
+            // DBファイルの存在チェックを行う
+            var result = await rootFolder.CheckExistsAsync(DatabaseFileName);
+            if (result == ExistenceCheckResult.NotFound)
+            {
+                // 存在しなかった場合、新たにDBファイルを作成しテーブルも併せて新規作成する
+                IFile file = await rootFolder.CreateFileAsync(DatabaseFileName, CreationCollisionOption.ReplaceExisting);
+                var connection = new SQLiteConnection(file.Path);
+                //connection.CreateTable<Item>();
+                return connection;
+            }
+            else
+            {
+                // 存在した場合、そのままコネクションを作成する
+                IFile file = await rootFolder.CreateFileAsync(DatabaseFileName, CreationCollisionOption.OpenIfExists);
+                return new SQLiteConnection(file.Path);
+            }
+        }
+    }
+}
