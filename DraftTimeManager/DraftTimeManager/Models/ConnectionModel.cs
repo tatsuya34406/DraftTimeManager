60a9119d2bc69d11fc9f70271d352a3553ea426e
tomopy03 <tomopy03@gmail.com>
2017-12-15 00:40:48 +0900

    create initial data and test data

--- DraftTimeManager/DraftTimeManager/Models/ConnectionModel.cs
+++ DraftTimeManager/DraftTimeManager/Models/ConnectionModel.cs
@@ -14,18 +14,14 @@ namespace DraftTimeManager.Models
 {
     public class ConnectionModel
     {
-        public SQLiteConnection conn;
+        string DatabaseFileName { get; }
         public ConnectionModel()
         {
-            var _conn = CreateConnection();
-            _conn.Wait();
-
-            conn = _conn.Result;
+            DatabaseFileName = "draftmanager.db3";
         }
 
         private async Task<SQLiteConnection> CreateConnection()
         {
-            const string DatabaseFileName = "draftmanager.db3";
             // ルートフォルダを取得する
             IFolder rootFolder = FileSystem.Current.LocalStorage;
             // DBファイルの存在チェックを行う
@@ -51,5 +47,109 @@ namespace DraftTimeManager.Models
                 return new SQLiteConnection(file.Path);
             }
         }
+
+        #region initialize
+        public async void DBInitialize()
+        {
+            IFolder rootFolder = FileSystem.Current.LocalStorage;
+
+            IFile new_file = await rootFolder.CreateFileAsync(DatabaseFileName, CreationCollisionOption.ReplaceExisting);
+            var connection = new SQLiteConnection(new_file.Path);
+            connection.CreateTable<Users>();
+            connection.CreateTable<Environments>();
+            connection.CreateTable<EnvironmentUserScore>();
+            connection.CreateTable<OpponentUserScore>();
+            connection.CreateTable<TempDraftResults>();
+            connection.CreateTable<DraftResults>();
+            connection.CreateTable<Settings>();
+
+            connection.InsertAll(InitialUsers());
+            connection.InsertAll(InitialEnvironments());
+            connection.Insert(InitialSettings());
+
+            connection.InsertAll(TestUsers());
+            connection.InsertAll(TestDraftResults());
+            connection.InsertAll(TestEnvironmentUserScore());
+            connection.InsertAll(TestOpponentUserScore());
+        }
+
+        private List<Users> InitialUsers()
+        {
+            return new List<Users>()
+            {
+                new Users() { User_Id = "1", User_Name = "Guest1" },
+                new Users() { User_Id = "2", User_Name = "Guest2" },
+                new Users() { User_Id = "3", User_Name = "Guest3" },
+                new Users() { User_Id = "4", User_Name = "Guest4" },
+                new Users() { User_Id = "5", User_Name = "Guest5" },
+                new Users() { User_Id = "6", User_Name = "Guest6" },
+                new Users() { User_Id = "7", User_Name = "Guest7" },
+                new Users() { User_Id = "8", User_Name = "Guest8" },
+            };
+        }
+
+        private List<Environments> InitialEnvironments()
+        {
+            return new List<Environments>()
+            {
+                new Environments() { Env_Id = "1", Env_Name = "KLD", Picks = 14 },
+                new Environments() { Env_Id = "2", Env_Name = "AER", Picks = 14 },
+                new Environments() { Env_Id = "3", Env_Name = "AKH", Picks = 14 },
+                new Environments() { Env_Id = "4", Env_Name = "HOU", Picks = 14 },
+                new Environments() { Env_Id = "5", Env_Name = "XLN", Picks = 14 },
+            };
+        }
+
+        private Settings InitialSettings()
+        {
+            return new Settings() { Volume = 50, Pick_Interval = 10, Picks = 14 };
+        }
+
+        private List<Users> TestUsers()
+        {
+            return new List<Users>()
+            {
+                new Users() { User_Id = "100", User_Name = "Tomohisa Itaya" },
+                new Users() { User_Id = "101", User_Name = "Tatsuya Nakata" },
+            };
+        }
+
+        private List<DraftResults> TestDraftResults()
+        {
+            var now = DateTime.Now;
+            return new List<DraftResults>()
+            {
+                new DraftResults() {
+                    Draft_Id = 1, Env_Id = "5", User_Id = "100",
+                    R1_Vs_User = "1", R1_Result = 1, R2_Vs_User = "3", R2_Result = 1, R3_Vs_User = "101", R3_Result = 1,
+                    Rank = 1, Pick_No = 1, Tournament_No = 1, Draft_Date = now },
+                new DraftResults() {
+                    Draft_Id = 1, Env_Id = "5", User_Id = "101",
+                    R1_Vs_User = "8", R1_Result = 1, R2_Vs_User = "6", R2_Result = 1, R3_Vs_User = "100", R3_Result = 0,
+                    Rank = 2, Pick_No = 8, Tournament_No = 1, Draft_Date = now },
+            };
+        }
+
+        private List<EnvironmentUserScore> TestEnvironmentUserScore()
+        {
+            return new List<EnvironmentUserScore>()
+            {
+                new EnvironmentUserScore() { Env_Id = "5", User_Id = "100",
+                    Cnt_3_0 = 1, Cnt_2_1 = 0, Cnt_1_2 = 0, Cnt_0_3 = 0, Cnt_Win = 3, Cnt_Lose = 0 },
+                new EnvironmentUserScore() { Env_Id = "5", User_Id = "101",
+                    Cnt_3_0 = 0, Cnt_2_1 = 1, Cnt_1_2 = 0, Cnt_0_3 = 0, Cnt_Win = 2, Cnt_Lose = 1 },
+            };
+        }
+
+        private List<OpponentUserScore> TestOpponentUserScore()
+        {
+            return new List<OpponentUserScore>()
+            {
+                new OpponentUserScore() { User_Id = "100", Vs_User_Id = "101", Cnt_Win = 1, Cnt_Lose = 0},
+                new OpponentUserScore() { User_Id = "101", Vs_User_Id = "100", Cnt_Win = 0, Cnt_Lose = 1},
+            };
+        }
+
+        #endregion initialize
     }
 }
