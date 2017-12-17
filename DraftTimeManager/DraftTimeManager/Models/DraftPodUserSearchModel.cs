d5305782cef6664e8d9453078daaaf84680ffacb
tomopy03 <tomopy03@gmail.com>
2017-12-17 21:10:39 +0900

    Create DraftPodUserSearchPage

---
+++ DraftTimeManager/DraftTimeManager/Models/DraftPodUserSearchModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+using DraftTimeManager.Interfaces;
+using DraftTimeManager.Entities;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using PropertyChanged;
+
+namespace DraftTimeManager.Models
+{
+    public class DraftPodUserSearchModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<Users> UserList { get; set; }
+        public Users SelectedUser { get; set; }
+        public string SearchText { get; set; }
+
+        public DraftPodUserSearchModel()
+        {
+            SearchText = "";
+            UserList = new ObservableCollection<Users>();
+        }
+
+        public void Search()
+        {
+            using (var conn = new ConnectionModel().CreateConnection())
+            {
+                UserList = new ObservableCollection<Users>(
+                    conn.Table<Users>().Where(x => x.User_Name.Contains(SearchText)).ToList()
+                );
+            }
+        }
+    }
+}
---
+++ DraftTimeManager/DraftTimeManager/Views/DraftPodUserSearchPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+using DraftTimeManager.Models;
+
+namespace DraftTimeManager.Views
+{
+    public partial class DraftPodUserSearchPage : ContentPage
+    {
+        public event EventHandler UserSelected;
+
+        public DraftPodUserSearchModel model;
+        public DraftPodUserSearchPage(DraftPodUserSearchModel searchModel)
+        {
+            InitializeComponent();
+
+            model = searchModel;
+            this.BindingContext = model;
+
+            background.Source = ImageSource.FromResource("DraftTimeManager.Images.cork-wallet.png");
+        }
+
+        private void UserSearchBar_SearchButtonPressed(object sender, EventArgs e)
+        {
+            model.Search();
+        }
+
+        private void SearchResultUser_ItemSelected(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected override void OnDisappearing()
+        {
+            if(UserSelected != null)
+            {
+                UserSelected(model, EventArgs.Empty);
+            }
+
+            base.OnDisappearing();
+        }
+    }
+}
