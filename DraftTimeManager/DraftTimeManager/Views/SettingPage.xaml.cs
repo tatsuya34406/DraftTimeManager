cda8ed81fd784e2b3ec04d89530cc17a31f20883
tomopy03 <tomopy03@gmail.com>
2017-12-19 21:58:00 +0900

    create SettingPage

---
+++ DraftTimeManager/DraftTimeManager/Views/SettingPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using DraftTimeManager.Models;
+
+namespace DraftTimeManager.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SettingPage : ContentPage
+    {
+        public SettingPage()
+        {
+            InitializeComponent();
+
+            background.Source = ImageSource.FromResource("DraftTimeManager.Images.cork-wallet.png");
+        }
+    }
+}
