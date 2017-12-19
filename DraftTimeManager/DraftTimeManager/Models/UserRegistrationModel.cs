a3dc933569f352afeaf8abe476efb5c90e326ed0
tomopy03 <tomopy03@gmail.com>
2017-12-19 23:28:38 +0900

    create UserRegistration page

---
+++ DraftTimeManager/DraftTimeManager/Models/UserRegistrationModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+using DraftTimeManager.Interfaces;
+using DraftTimeManager.Entities;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using PropertyChanged;
+
+namespace DraftTimeManager.Models
+{
+    public class UserRegistrationModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string SearchText { get; set; }
+        public ObservableCollection<Users> UserList { get; set; }
+
+        public UserRegistrationModel()
+        {
+            SearchReset();
+        }
+
+        public void Search()
+        {
+            using (var conn = new ConnectionModel().CreateConnection())
+            {
+                UserList = new ObservableCollection<Users>(
+                    conn.Table<Users>().Where(x => x.User_Name.Contains(SearchText) && !x.Guest_Flg).ToList());
+            }
+        }
+
+        public void SearchReset()
+        {
+            using (var conn = new ConnectionModel().CreateConnection())
+            {
+                UserList = new ObservableCollection<Users>(
+                    conn.Table<Users>().Where(x => !x.Guest_Flg).ToList());
+            }
+        }
+    }
+}
---
+++ DraftTimeManager/DraftTimeManager/Views/UserRegistration.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using DraftTimeManager.Models;
+
+namespace DraftTimeManager.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class UserRegistration : ContentPage
+    {
+        UserRegistrationModel model;
+        public UserRegistration()
+        {
+            model = new UserRegistrationModel();
+            this.BindingContext = model;
+
+            InitializeComponent();
+
+            background.Source = ImageSource.FromResource("DraftTimeManager.Images.cork-wallet.png");
+        }
+
+        private void UserSearchBar_SearchButtonPressed(object sender, EventArgs e)
+        {
+            model.Search();
+        }
+
+        private void UserSearchBar_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(model.SearchText)) model.SearchReset();
+        }
+    }
+}
