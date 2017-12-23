fa16454661133e5e76fb3506bc3ada988923f2d8
tomopy03 <tomopy03@gmail.com>
2017-12-23 16:26:52 +0900

    create environment setting page

---
+++ DraftTimeManager/DraftTimeManager/Models/EnvironmentSettingModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+using DraftTimeManager.Interfaces;
+using DraftTimeManager.Entities;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using PropertyChanged;
+
+namespace DraftTimeManager.Models
+{
+    public class EnvironmentSettingModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public EnvironmentSettingModel()
+        {
+        }
+    }
+}
--- DraftTimeManager/DraftTimeManager/Views/MenuMaster.xaml.cs
+++ DraftTimeManager/DraftTimeManager/Views/MenuMaster.xaml.cs
@@ -38,7 +38,7 @@ namespace DraftTimeManager.Views
                     new MenuMenuItem { Id = 0, Title = "DraftTimer", TargetType = typeof(TimerPage) },
                     new MenuMenuItem { Id = 1, Title = "PodCreate" },
                     new MenuMenuItem { Id = 2, Title = "UserRegistration" },
-                    new MenuMenuItem { Id = 3, Title = "EnvironmentRegistration" },
+                    new MenuMenuItem { Id = 3, Title = "EnvironmentRegistration", TargetType = typeof(EnvironmentSettingPage) },
                     new MenuMenuItem { Id = 4, Title = "OverallResults" },
                     new MenuMenuItem { Id = 5, Title = "PersonalResults" },
                     new MenuMenuItem { Id = 6, Title = "DraftHistory" },
