61181fc5333bb62d671c443f4cba576caac7b5b7
tomopy03 <tomopy03@gmail.com>
2017-12-23 19:07:07 +0900

    create environment edit page

---
+++ DraftTimeManager/DraftTimeManager/Models/EnvironmentEditModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+using DraftTimeManager.Interfaces;
+using DraftTimeManager.Entities;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using PropertyChanged;
+
+namespace DraftTimeManager.Models
+{
+    public class EnvironmentEditModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private int EnvId;
+        public string EnvName { get; set; }
+        public int Picks { get; set; }
+
+        public bool IsRegist => !string.IsNullOrWhiteSpace(EnvName);
+
+        public EnvironmentEditModel()
+        {
+            EnvId = 0;
+            EnvName = string.Empty;
+            Picks = 14;
+        }
+
+        public EnvironmentEditModel(int envid)
+        {
+            using (var conn = new ConnectionModel().CreateConnection())
+            {
+                var env = conn.Get<Environments>(envid);
+
+                EnvId = env.Env_Id;
+                EnvName = env.Env_Name;
+                Picks = env.Picks;
+            }
+        }
+
+        public void EnvironmentRegist()
+        {
+            var env = new Environments
+            {
+                Env_Id = EnvId,
+                Env_Name = EnvName,
+                Picks = Picks,
+                Default_Flg = false,
+                Delete_Flg = false
+            };
+
+            using (var conn = new ConnectionModel().CreateConnection())
+            {
+                try
+                {
+                    conn.BeginTransaction();
+
+                    if (env.Env_Id == 0)
+                    {
+                        conn.Insert(env);
+                    }
+                    else
+                    {
+                        conn.Update(env);
+                    }
+
+                    conn.Commit();
+                }
+                catch
+                {
+                    conn.Rollback();
+                }
+            }
+        }
+    }
+}
---
+++ DraftTimeManager/DraftTimeManager/Views/EnvironmentEditPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using DraftTimeManager.Models;
+using DraftTimeManager.Entities;
+
+namespace DraftTimeManager.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EnvironmentEditPage : ContentPage
+    {
+        public event EventHandler EnvironmentUpdate;
+        EnvironmentEditModel model;
+        public EnvironmentEditPage()
+        {
+            model = new EnvironmentEditModel();
+            this.BindingContext = model;
+
+            InitializeComponent();
+
+            background.Source = ImageSource.FromResource("DraftTimeManager.Images.cork-wallet.png");
+        }
+
+        public EnvironmentEditPage(int envid)
+        {
+            model = new EnvironmentEditModel(envid);
+            this.BindingContext = model;
+
+            InitializeComponent();
+
+            background.Source = ImageSource.FromResource("DraftTimeManager.Images.cork-wallet.png");
+        }
+
+        async void btnRegist_Clicked(object sender, System.EventArgs e)
+        {
+            if (model.IsRegist)
+            {
+                model.EnvironmentRegist();
+                EnvironmentUpdate(model, e);
+                await Navigation.PopAsync(true);
+            }
+        }
+    }
+}
