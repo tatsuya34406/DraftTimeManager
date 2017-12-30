45bd04a9937e80e455c7283a72f88d6f79f37099
tomopy03 <tomopy03@gmail.com>
2017-12-30 23:01:55 +0900

    add personal results page

---
+++ DraftTimeManager/DraftTimeManager/Models/PersonalResultModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+using DraftTimeManager.Interfaces;
+using DraftTimeManager.Entities;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using PropertyChanged;
+
+namespace DraftTimeManager.Models
+{
+    public class PersonalResultModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<GroupingItem> OverallScoreList { get; set; }
+
+        public PersonalResultModel()
+        {
+        }
+
+        public class PersonalScore
+        {
+            public int Rank { get; set; }
+            public string UserName { get; set; }
+            public int wins { get; set; }
+            public int loses { get; set; }
+            public decimal percentage => wins + loses == 0 ? 0m : Math.Round(((decimal)wins / ((decimal)wins + (decimal)loses)) * 100m, 3, MidpointRounding.AwayFromZero);
+            public string Score { get; set; }
+        }
+
+        public class GroupingItem : ObservableCollection<PersonalScore>
+        {
+            public string SectionLabel { get; set; }
+        }
+    }
+}
---
+++ DraftTimeManager/DraftTimeManager/Views/PersonalResultsPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace DraftTimeManager.Views
+{
+    public partial class PersonalResultsPage : ContentPage
+    {
+        public PersonalResultsPage()
+        {
+            InitializeComponent();
+
+            background.Source = ImageSource.FromResource("DraftTimeManager.Images.cork-wallet.png");
+        }
+    }
+}
