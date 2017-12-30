a75d26a5c41a69dcd1dc873ab45f09af9a7828c0
tomopy03 <tomopy03@gmail.com>
2017-12-30 09:03:24 +0900

    add overall results page

---
+++ DraftTimeManager/DraftTimeManager/Views/OverallResultsPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace DraftTimeManager.Views
+{
+    public partial class OverallResultsPage : ContentPage
+    {
+        public OverallResultsPage()
+        {
+            InitializeComponent();
+
+            background.Source = ImageSource.FromResource("DraftTimeManager.Images.cork-wallet.png");
+        }
+    }
+}
