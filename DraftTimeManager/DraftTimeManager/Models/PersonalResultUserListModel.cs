3cf2e736d3ca71853ce9e19c0d828aa99acba192
tomopy03 <tomopy03@gmail.com>
2017-12-31 16:06:46 +0900

    create personal result user list page

---
+++ DraftTimeManager/DraftTimeManager/Models/PersonalResultUserListModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+using DraftTimeManager.Interfaces;
+using DraftTimeManager.Entities;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using PropertyChanged;
+
+namespace DraftTimeManager.Models
+{
+    public class PersonalResultUserListModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string SearchText { get; set; }
+        public ObservableCollection<Users> UserList { get; set; }
+
+        public PersonalResultUserListModel()
+        {
+            SearchText = string.Empty;
+            Search();
+        }
+
+        public void Search()
+        {
+            using (var conn = new ConnectionModel().CreateConnection())
+            {
+                if (string.IsNullOrWhiteSpace(SearchText))
+                {
+                    UserList = new ObservableCollection<Users>(
+                        conn.Table<Users>()
+                            .Where(x => !x.Guest_Flg && !x.Delete_Flg)
+                            .ToList());
+                }
+                else
+                {
+                    UserList = new ObservableCollection<Users>(
+                        conn.Table<Users>()
+                            .Where(x => x.User_Name.Contains(SearchText) && !x.Guest_Flg && !x.Delete_Flg)
+                            .ToList());
+                }
+            }
+        }
+    }
+}
---
+++ DraftTimeManager/DraftTimeManager/Views/PersonalResultsUserListPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using DraftTimeManager.Models;
+
+namespace DraftTimeManager.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PersonalResultsUserListPage : ContentPage
+    {
+        PersonalResultUserListModel model;
+        public PersonalResultsUserListPage()
+        {
+            InitializeComponent();
+
+            model = new PersonalResultUserListModel();
+            this.BindingContext = model;
+
+            background.Source = ImageSource.FromResource("DraftTimeManager.Images.cork-wallet.png");
+        }
+
+        private void UserSearchBar_SearchButtonPressed(object sender, EventArgs e)
+        {
+            model.Search();
+        }
+
+        private void UserSearchBar_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(model.SearchText)) model.Search();
+        }
+
+        private async void SearchResultUser_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (SearchResultUser.SelectedItem == null) return;
+
+            var page = new PersonalResultsPage();
+
+            await Navigation.PushAsync(page, true);
+
+            SearchResultUser.SelectedItem = null;
+        }
+    }
+}
